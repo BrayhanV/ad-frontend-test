bcdd16dd6fc05ac0509cf845e8f22bebb667afd5
Brayhan Villalba <brayhanalberto@gmail.com>
2025-03-12 19:37:48 -0300

    Setting up elements for ui theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        "area-normal": ["24px", "24px"],
+        xs: ["20px", "24px"],
+        xl: ["24px", "28px"], 
+        "2xl": ["36px", "40px"]
+      },
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        neutral: {
+          500: "var(--color-neutral-500)",
+          700: "var(--color-neutral-700)",
+        },
+        surface: {
+          primary: "var(--surface-primary)",
+          secondary: "var(--surface-secondary)",
+        }
+      },
+      stroke: {
+        primary: "var(--icon-primary)",
+        active: "var(--icon-active)",
+      },
     },
   },
   plugins: [],
