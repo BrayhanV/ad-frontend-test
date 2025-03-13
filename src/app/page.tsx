b5683b65872f9130021ebf656848e111850ff0cc
Brayhan Villalba <brayhanalberto@gmail.com>
2025-03-12 23:37:17 -0300

    List games on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 import { AtSelect } from "@/components/atoms/AtSelect";
+import { OrProductListing } from "@/components/organisms/OrProductListing";
 import { getGames } from "@/services/games";
 
 export default async function Home() {
   const gamesData = await getGames("", 1);
 
   return (
-    <main className='flex min-h-full px-6 md:px-32'>
+    <main className='flex flex-col min-h-full px-6 md:px-32'>
       <div className="flex flex-col min-w-full gap-8 py-6 md:py-12">
-        <h2 className="text-primary font-bold text-xl md:text-2xl">TOP SELLERS</h2>
+        <h1 className="text-primary font-bold text-xl md:text-2xl">TOP SELLERS</h1>
         
         <div className="flex flex-row gap-6 min-w-full md:min-w-fit md:max-w-fit items-center md:self-end">
-          <label htmlFor="genre" className="text-xs font-bold text-primary">Genre</label>
+          <label htmlFor="genre" className="text-lg font-bold text-primary">Genre</label>
           <div className="h-[22px] w-px border-solid border-r border-primary" />
           <AtSelect id="genre" name="genre" options={["All", ...gamesData.availableFilters]} />
         </div>
       </div>
+      <OrProductListing products={gamesData.games} />
     </main>
   )
 }
--- src/components/organisms/OrHeader.tsx
+++ src/components/organisms/OrHeader.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import CartIcon from '../../../public/cart.svg';
+import Link from 'next/link';
 
 export const OrHeader = () => {
   return (
     <header className='flex justify-between items-center px-6 md:px-32 py-5 bg-surface-secondary'>
-      <h2 className='text-area-normal tracking-wide font-bold text-secondary'>GamerShop</h2>
+      <Link href="/" className='text-area-normal tracking-wide font-bold text-secondary'>GamerShop</Link>
       <nav>
         <ul className='flex space-x-4'>
           <li>
