5dd089983f8a08f02d77b368bcd3f52b22561269
Brayhan Villalba <brayhanalberto@gmail.com>
2025-03-13 02:26:35 -0300

    Fix button responsive style

--- src/components/atoms/AtButton/index.tsx
+++ src/components/atoms/AtButton/index.tsx
@@ -8,7 +8,7 @@ export const AtButton = ({ variant, color, children, onClick }: AtButtonProps) =
       className={`
         px-6 py-4 gap-2 rounded-lg font-bold tracking-[0.5px] 
         transition-all duration-300 ease-in-out 
-        ${variant ? AtButtonClasses[variant] : "text-xs w-fit md:h-[56px] md:text-sm"} 
+        ${variant ? AtButtonClasses[variant] : "text-xs w-full md:w-fit md:h-[56px] md:text-sm"} 
         ${AtButtonClasses[color]}
       `}
       onClick={onClick}
