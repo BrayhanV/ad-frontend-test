a75fb03cf28883f0c6362e6c9a99dd5d2259f995
Brayhan Villalba <brayhanalberto@gmail.com>
2025-03-14 15:49:27 -0300

    fix(storage): :hammer: Fix product persistence
    Fix product persistence on cart store

--- src/app/stores/cart/index.ts
+++ src/app/stores/cart/index.ts
@@ -9,37 +9,42 @@ const defaultInitState: CartState = {
 };
 
 const useCartStore = create<CartStore>()(
-    persist(
-      (set) => ({
-        ...defaultInitState,
-        addGame: (game) => set((state) => {
-          const newGames = new Map(state.products).set(game.id, game);
-          return {
-            products: newGames,
-            total: state.total + game.price
-          };
-        }),
-        removeGame: (gameId) => set((state) => {
-          const game = state.products.get(gameId);
-          if (!game) return state;
-
-          const newGames = new Map(state.products);
-          newGames.delete(gameId);
-          return {
-            products: newGames,
-            total: state.total - game.price
-          };
-        }),
-        clearCart: () => set(() => defaultInitState),
+  persist(
+    (set) => ({
+      ...defaultInitState,
+      addGame: (game) => set((state) => {
+        const newGames = new Map(state.products).set(game.id, game);
+        return {
+          products: newGames,
+          total: state.total + game.price,
+        };
       }),
-      {
-        name: 'cart-storage',
-        partialize: (state) => ({ 
-          games: new Map(state.products),
-          total: state.total
-        }),
-      }
-    )
-  );
+      removeGame: (gameId) => set((state) => {
+        const game = state.products.get(gameId);
+        if (!game) return state;
+
+        const newGames = new Map(state.products);
+        newGames.delete(gameId);
+        return {
+          products: newGames,
+          total: state.total - game.price,
+        };
+      }),
+      clearCart: () => set(() => defaultInitState),
+    }),
+    {
+      name: 'cart-storage',
+      partialize: (state) => ({
+        products: Array.from(state.products.entries()),
+        total: state.total,
+      }),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.products = new Map(state.products);
+        }
+      },
+    }
+  )
+);
 
 export default useCartStore;
