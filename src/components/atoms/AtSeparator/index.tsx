5c880e62ea62cf1b03f0958b2ccc4d2326d4caff
Brayhan Villalba <brayhanalberto@gmail.com>
2025-03-14 22:03:12 -0300

    feat(Atoms): :art: AtSeperator component added

---
+++ src/components/atoms/AtSeparator/index.tsx
@@ -0,0 +1,7 @@
+import { AtSeparatorProps } from "./type"
+
+export const AtSeparator = ({ color, height, className }: AtSeparatorProps) => {
+  return (
+    <hr className={`w-full h-[${height}px] bg-[${color}] ${className}`} />
+  )
+}
---
+++ src/components/atoms/AtSeparator/type.ts
@@ -0,0 +1,7 @@
+
+
+export interface AtSeparatorProps {
+  color: string;
+  height: 1 | 2 | 3 | 4;
+  className?: string;
+}
