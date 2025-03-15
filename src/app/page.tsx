a4b9547f5e97bb5b7baf6fca57b493429761a714
Brayhan Villalba <brayhanalberto@gmail.com>
2025-03-15 14:12:09 -0300

    fix: :hammer: Fix error on build
    Suspense added on Home page to handle useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { TmHome } from "@/components/templates/TmHome";
+import { Suspense } from "react";
 
 export default async function Home() {
-  return <TmHome />;
+  return (
+    <Suspense>
+      <TmHome />
+    </Suspense>
+  );
 }
